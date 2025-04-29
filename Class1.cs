f63aa94b2d612f26e5390183144ba793d32c1f52
fifi <fifi@DESKTOP-CDGKJC6>
2025-04-29 21:25:29 +0200

    Dodaj pliki projektów.

---
+++ Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad4
+{
+    public class BankAccount
+    {
+        private string accountNumber;
+        private decimal balance;
+
+        public BankAccount(string accountNumber, decimal balance)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+        }
+
+        public string GetAccountNumber()
+        {
+            return accountNumber;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Kwota wpłaty musi być większa od zera.");
+            }
+            else
+            {
+                balance += amount;
+                Console.WriteLine("Wpłacono: " + amount);
+                Console.WriteLine("Saldo po wpłacie: " + balance);
+            }
+
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Kwota wypłaty musi być większa od zera.");
+            }
+            else if (amount > balance)
+            {
+                Console.WriteLine("Niewystarczające środki na koncie.");
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine("Wypłacono: " + amount);
+                Console.WriteLine("Saldo po wypłacie: " + balance);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("1234567890", 1000);
+
+            Console.WriteLine("Numer konta: " + account.GetAccountNumber());
+            Console.WriteLine("Saldo początkowe: " + account.GetBalance());
+            account.Deposit(1000);
+            account.Withdraw(20);
+        }
+    }
+}
